579312fda763c9768f2fff2a5c16b189a5de5d19
Teja Harsoni <teza.harsony230394@gmail.com>
2020-08-14 21:15:08 +0700

    filter start date and stop date so can calculate degree

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const csv = require("csv-parser");
 const fs = require("fs");
 var multer = require("multer");
+const { start } = require("repl");
 var upload = multer({ dest: "uploads/" });
 
-/* GET home page. */
 router.post("/", upload.single("file"), function (req, res, next) {
-  console.log(req.file);
   const results = [];
   const input = [];
   fs.createReadStream(req.file.path)
@@ -18,9 +17,26 @@ router.post("/", upload.single("file"), function (req, res, next) {
         .pipe(csv())
         .on("data", (data) => results.push(data))
         .on("end", () => {
-          console.log("file uploaded success");
-          console.log(results[0]);
-          console.log(input);
+          for (let i = 0; i < input.length; i++) {
+            let startIndex = results.findIndex(
+              (data) => data.Date === input[i].startDate
+            );
+            let stopIndex = results.findIndex(
+              (data) => data.Date === input[i].stopDate
+            );
+            let planet = input[i].planetNum;
+
+            for (let j = startIndex; j < stopIndex; j++) {
+              console.log(
+                typeof parseInt(input[0].planetLong),
+                typeof parseInt(results[j][planet])
+              );
+              console.log(
+                "ini result",
+                parseInt(input[0].planetLong) - parseInt(results[j][planet])
+              );
+            }
+          }
         });
     });
 });
