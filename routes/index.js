73739efc4b498aa52a2e4cc2df175825d0d7fa73
Teja Harsoni <teza.harsony230394@gmail.com>
2020-08-14 19:52:09 +0700

    make sample input

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const csv = require('csv-parser')
-const fs = require('fs')
-var multer = require('multer')
-var upload = multer({ dest: 'uploads/' })
+const csv = require("csv-parser");
+const fs = require("fs");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 /* GET home page. */
-router.post('/', upload.single('file'), function (req, res, next) {
-  console.log(req.file)
+router.post("/", upload.single("file"), function (req, res, next) {
+  console.log(req.file);
   const results = [];
   const input = [];
   fs.createReadStream(req.file.path)
     .pipe(csv())
-    .on('data', (data) => input.push(data))
-    .on('end', () => {
-      console.log('oke');
-      fs.createReadStream('data_fix.csv')
+    .on("data", (data) => input.push(data))
+    .on("end", () => {
+      fs.createReadStream("data_fix.csv")
         .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          console.log('ini input ==>', input[0])
-        })
+        .on("data", (data) => results.push(data))
+        .on("end", () => {
+          console.log("file uploaded success");
+          console.log(results[0]);
+          console.log(input);
+        });
     });
-})
+});
 
 module.exports = router;
