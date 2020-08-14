99704f6a8fe23c18429200a9d4b88e5a8c7bc639
teza harsony <teza.harsony230394@gmail.com>
2020-08-14 16:50:18 +0700

    upload file request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const csv = require('csv-parser')
+const fs = require('fs')
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/', upload.single('file'), function (req, res, next) {
+  console.log(req.file)
+  const results = [];
+  const input = [];
+  fs.createReadStream(req.file.path)
+    .pipe(csv())
+    .on('data', (data) => input.push(data))
+    .on('end', () => {
+      console.log('oke');
+      fs.createReadStream('data_fix.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+          console.log('ini input ==>', input[0])
+        })
+    });
+})
 
 module.exports = router;
